d142fede1627c469fdbacae4dde5ca7965b6cc1f
Vajay Attila <vajay.attila@gmail.com>
2020-02-20 18:00:03 +0100

    create rbplayer object
    create rbplayer object

--- js/audioplayer.js
+++ js/audioplayer.js
@@ -4,197 +4,172 @@
  *  Project home: https://github.com/vajayattila/RockBoxRadioPlayer
  *	@author Vajay Attila (vajay.attila@gmail.com)
  *  @copyright MIT License (MIT)
- *  @date 2020.02.05-2020.02.10
- *  @version 1.0.0.0
+ *  @date 2020.02.05-2020.02.20
+ *  @version 1.0.0.1
  */
 
-var player = null;  
-
 /*
+sample parameters
 var urlBase= "https://vps.vyata.hu:9011"; // https://rockboxradio.wixsite.com/official
 var playerCaption= "RockBoxRadio Player"; // https://rockboxradio.wixsite.com/official
 var sourceIndex=0; // https://rockboxradio.wixsite.com/official
-
-
-var urlBase= "http://stream.diazol.hu:35200"; // http://rockerradio.online/
-var playerCaption= "Rocker Radio"; // http://rockerradio.online/
-var sourceIndex=1; // http://rockerradio.online/
-
 var urlParams= "/stream?type=.mp3";
 */
-var urlBase=""; 
-var playerCaption=""; 
-var sourceIndex=0;
-var urlParams= "";
-var songInfoRefreshRate=4000;
-var maxSongTitleLength=60;  
-var forceSongShowTitle=false;
-var player = null;        
-var info = null; 
-var xmlhttp = null;
-var serverInfo=null;        
-var infoDiv = null;  
-var statusDiv = null;           
-var timeR = null;
-var playButton = null;
-var stopButton = null;
-var pauseInterval=null;
-var forceDecodeUtf8=false;
 
-function setForceDecodeUtf8(){
-    forceDecodeUtf8=true;
-}
-
-function forceUnicodeEncoding(str) {
-    if(forceDecodeUtf8==true){
-        str=decodeURIComponent(escape(str));
-    }
-    return str;
-}
-  
-function setSongTitle(title){
-    statusDiv.style.display="none";    
-    infoDiv.style.display="flex"
-    infoDiv.innerHTML=forceUnicodeEncoding(title);    
-}
-
-function setStausText(title){
-    infoDiv.style.display="none"    
-    statusDiv.style.display="flex";    
-    statusDiv.innerHTML=title;  
-
-}
-
-function setup(pUrlBase, pUrlParams, pPlayerCaption, pSourceIndex){
-    urlBase= pUrlBase;
-    playerCaption= pPlayerCaption;
-    sourceIndex=pSourceIndex;   
-    urlParams= pUrlParams;
-    player = document.getElementById("rockboxPlayer");     
-    info = document.getElementById("actionDiv"); 
-    caption = document.getElementById("caption");
-    caption.innerHTML=playerCaption;
-    xmlhttp = new XMLHttpRequest();
-    serverInfo=null;        
-    infoDiv = document.getElementById("infoDiv"); 
-    statusDiv=document.getElementById("statusDiv");     
-    statusDiv.style.display="none";        
-    timeR = setInterval(function(){
-        if((!player.paused && pauseInterval==null) || forceSongShowTitle===true){
-            xmlhttp.open("GET", urlBase.concat("/", "status-json.xsl"), true);
-            xmlhttp.send();    
-        }else{
-            // do nothing                
-        }         
-    }, songInfoRefreshRate);
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            serverInfo = JSON.parse(this.responseText);
-            if(Array.isArray(serverInfo.icestats.source)){
-                setSongTitle(serverInfo.icestats.source[sourceIndex].title);
+var rbplayer={
+    urlBase: "", 
+    playerCaption: "", 
+    sourceIndex: 0,
+    urlParams: "",
+    songInfoRefreshRate: 4000,
+    maxSongTitleLength: 60,  
+    forceSongShowTitle: false,
+    player: null,        
+    info: null, 
+    xmlhttp: null,
+    serverInfo: null,        
+    infoDiv: null,  
+    statusDiv: null,           
+    timeR: null,
+    playButton: null,
+    stopButton: null,
+    pauseInterval: null,
+    forceDecodeUtf8: false,
+    setForceDecodeUtf8: function(){
+        forceDecodeUtf8=true;
+    },
+    forceUnicodeEncoding: function(str) {
+        if(rbplayer.forceDecodeUtf8==true){
+            str=rbplayer.decodeURIComponent(escape(str));
+        }
+        return str;
+    },
+    setSongTitle: function (title){
+        statusDiv.style.display="none";    
+        infoDiv.style.display="flex"
+        infoDiv.innerHTML=rbplayer.forceUnicodeEncoding(title);    
+    },
+    setStausText: function (title){
+        infoDiv.style.display="none"    
+        statusDiv.style.display="flex";    
+        statusDiv.innerHTML=title;  
+    },
+    setup: function(pUrlBase, pUrlParams, pPlayerCaption, pSourceIndex){
+        rbplayer.urlBase= pUrlBase;
+        rbplayer.playerCaption= pPlayerCaption;
+        rbplayer.sourceIndex=pSourceIndex;   
+        rbplayer.urlParams= pUrlParams;
+        rbplayer.player = document.getElementById("radio");     
+        rbplayer.info = document.getElementById("actionDiv"); 
+        rbplayer.caption = document.getElementById("caption");
+        rbplayer.caption.innerHTML=rbplayer.playerCaption;
+        rbplayer.xmlhttp = new XMLHttpRequest();
+        rbplayer.serverInfo=null;        
+        rbplayer.infoDiv = document.getElementById("infoDiv"); 
+        rbplayer.statusDiv=document.getElementById("statusDiv");     
+        rbplayer.statusDiv.style.display="none";        
+        rbplayer.timeR = setInterval(function(){
+            if(
+                (!rbplayer.player.paused && rbplayer.pauseInterval==null) || 
+                rbplayer.forceSongShowTitle===true
+            ){
+                rbplayer.xmlhttp.open("GET", rbplayer.urlBase.concat("/", "status-json.xsl"), true);
+                rbplayer.xmlhttp.send();    
             }else{
-                setSongTitle(serverInfo.icestats.source.title);
-                //setSongTitle("aaaaaaaaaa bbbbbbbbbb cccccc ccccc dddddddddd eeeeeeeeee");                 
+                // do nothing                
+            }         
+        }, rbplayer.songInfoRefreshRate);
+        rbplayer.xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                rbplayer.serverInfo = JSON.parse(this.responseText);
+                if(Array.isArray(rbplayer.serverInfo.icestats.source)){
+                    rbplayer.setSongTitle(rbplayer.serverInfo.icestats.source[rbplayer.sourceIndex].title);
+                }else{
+                    rbplayer.setSongTitle(rbplayer.serverInfo.icestats.source.title);                
+                }
+            }
+        };
+        playButton = document.getElementById("playButton");
+        stopButton = document.getElementById("stopButton");        
+
+        rbplayer.player.oncanplay=function() {
+            playButton.disabled=false;            
+            stopButton.disabled=true;
+            if(rbplayer.player.paused){
+                rbplayer.setSongTitle("Press '>' to play.");
+            }
+        };              
+        rbplayer.player.onplaying=function() { 
+            playButton.disabled=true;            
+            stopButton.disabled=false;            
+        };
+        rbplayer.player.onpause=function() {
+            playButton.disabled=false;            
+            stopButton.disabled=true;     
+            rbplayer.setStausText("Stopped");       
+        };   
+        rbplayer.player.onloadstart=function() {
+            playButton.disabled=false;            
+            stopButton.disabled=true;                
+            rbplayer.setStausText("Loading..."); 
+        };                
+    },
+    playRock: function(){
+        rbplayer.player.load();
+        rbplayer.player.play();
+    },
+    pauseRock: function(){
+        rbplayer.player.pause();
+    },
+    setupPlayer: function(){
+        var audio=document.getElementById("radio");
+        audio.src=rbplayer.urlBase.concat(rbplayer.urlParams);
+        audio.type="audio/mpeg";
+        rbplayer.setVolumeButtons();
+    },
+    setPausedTimer: function(){
+        if(rbplayer.player.paused){
+            if(rbplayer.pauseInterval==null){
+                rbplayer.pauseInterval=setInterval(function(){
+                    if(rbplayer.player.paused){
+                        rbplayer.setStausText("Stopped");                    
+                    } 
+                    clearInterval(rbplayer.pauseInterval);
+                    rbplayer.pauseInterval=null;  
+                }, rbplayer.songInfoRefreshRate);                
             }
         }
-    };
-    playButton = document.getElementById("playButton");
-    stopButton = document.getElementById("stopButton");        
-
-    player.oncanplay=function() {
-        playButton.disabled=false;            
-        stopButton.disabled=true;
-        //info.innerHTML=playerCaption;
-        if(player.paused){
-            setSongTitle("Press '>' to play.");
-            //infoDiv.innerHTML="Press '>' to play.";
+    },
+    volumeString: function(){
+        return "Volume: " + rbplayer.player.volume*100 + "%";
+    },
+    setVolumeButtons: function(){
+        var volUp=document.getElementById("volUp");  
+        var volDown=document.getElementById("volDown");     
+        if(volUp!=null){ 
+            volUp.disabled=rbplayer.player.volume==1;
         }
-    };              
-    player.onplaying=function() { 
-        playButton.disabled=true;            
-        stopButton.disabled=false;            
-    };
-    player.onpause=function() {
-        playButton.disabled=false;            
-        stopButton.disabled=true;     
-        setStausText("Stopped");       
-        //infoDiv.innerHTML="Stopped";        
-    };   
-    player.onloadstart=function() {
-        playButton.disabled=false;            
-        stopButton.disabled=true;                
-        //infoDiv.innerHTML="Loading...";
-        setStausText("Loading..."); 
-    };                
-}
-
-function playRock(){
-    player.load();
-    player.play();
-}
-
-function pauseRock(){
-    player.pause();
-}  
-
-function setupPlayer(){
-    var audio=document.getElementById("rockboxPlayer");
-    audio.src=urlBase.concat(urlParams);
-    audio.type="audio/mpeg";
-    setVolumeButtons();
-    //playRock();    
-}
-
-function setPausedTimer(){
-    if(player.paused){
-        if(pauseInterval==null){
-            pauseInterval=setInterval(function(){
-                if(player.paused){
-                    //infoDiv.innerHTML="Stopped";
-                    setStausText("Stopped");                    
-                } 
-                clearInterval(pauseInterval);
-                pauseInterval=null;  
-            }, songInfoRefreshRate);                
+        if(volDown!=null){
+            volDown.disabled=rbplayer.player.volume==0;
         }
+    },
+    volUp: function(){
+        if(rbplayer.player.volume<1){
+            rbplayer.setPausedTimer();
+            rbplayer.player.volume=Math.round((rbplayer.player.volume+0.1)*10)/10;
+            rbplayer.setStausText(rbplayer.volumeString());
+            rbplayer.setVolumeButtons();
+        }
+    },  
+    volDown: function (){
+        if(0<rbplayer.player.volume){
+            rbplayer.setPausedTimer();        
+            rbplayer.player.volume=Math.round((rbplayer.player.volume-0.1)*10)/10;        
+            rbplayer.setStausText(rbplayer.volumeString());
+            rbplayer.setVolumeButtons();        
+        }
+    },
+    forceSongTitle: function(){
+        rbplayer.forceSongShowTitle=true;
     }
-}
-
-function volumeString(){
-    return "Volume: " + player.volume*100 + "%";
-}
-
-function setVolumeButtons(){
-    var volUp=document.getElementById("volUp");  
-    var volDown=document.getElementById("volDown");     
-    if(volUp!=null){ 
-        volUp.disabled=player.volume==1;
-    }
-    if(volDown!=null){
-        volDown.disabled=player.volume==0;
-    }
-}
-
-function volUp(){
-    if(player.volume<1){
-        setPausedTimer();
-        player.volume=Math.round((player.volume+0.1)*10)/10;
-        //infoDiv.innerHTML=volumeString();
-        setStausText(volumeString());
-        setVolumeButtons();
-    }
-}  
-
-function volDown(){
-    if(0<player.volume){
-        setPausedTimer();        
-        player.volume=Math.round((player.volume-0.1)*10)/10;        
-        //infoDiv.innerHTML=volumeString();
-        setStausText(volumeString());
-        setVolumeButtons();        
-    }
-}  
-
-function forceSongTitle(){
-    forceSongShowTitle=true;
 }
